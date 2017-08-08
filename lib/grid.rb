1c1f261a72e119eb23be348836e13463e16fda59
Aaron <aaronrod@gmail.com>
2017-08-08 21:46:07 +0100

    Adds a query method - Queries the user to input co-ordinates - Converts the coordinates to an integer -Finds the closest tickets

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,5 +1,13 @@
 class Grid
-	def initialize(events)
+  def initialize(events)
     @events = events
   end
+
+  def query()
+    puts "Please Input Coordinates:\n"
+    location = gets.chomp
+    location = location.split(",").map{|element| element.to_i}
+    find_cheap_close_ticket(location)
+  end
+  
 end
