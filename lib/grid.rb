cdaadb67c68509ba94dbe4073ed62d1fcae9891b
Aaron <aaronrod@gmail.com>
2017-08-08 21:48:21 +0100

    Adds methods to find closest event and find cheapest tickets

--- lib/grid.rb
+++ lib/grid.rb
@@ -9,5 +9,17 @@ class Grid
     location = location.split(",").map{|element| element.to_i}
     find_cheap_close_ticket(location)
   end
+
+  def find_closest_event(location, num=5)
+    sorted_events = @events.sort_by {|event| event.distance_to(location)}
+    sorted_events[0..num-1]
+  end
+
+  def find_cheap_close_ticket(location)
+    self.find_closest_event(location).each do |event|
+      puts "Event id:#{event.id} Ticket price: #{event.find_cheapest_ticket().price} Distance: #{event.distance_to(location)}\n"
+    end
+    nil
+  end
   
 end
