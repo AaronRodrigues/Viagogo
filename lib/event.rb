02229b527062695f58950b3df9ed4275d44f77a6
Aaron <aaronrod@gmail.com>
2017-08-08 20:17:59 +0100

    Event initializes with id, location and tickets

--- lib/event.rb
+++ lib/event.rb
@@ -1,6 +1,7 @@
 require_relative '../ticket'
 
 class Event
+	attr_reader :id, :location, :tickets
 	def initialize(id, location, tickets)
 		@id = id
 		@location = location
@@ -11,4 +12,11 @@ class Event
     	return (location[0] - @location[0]).abs + (location[1] - @location[1]).abs
   	end
 
+  	def find_cheapest_ticket()
+    	@tickets.sort_by {|ticket| ticket.price}[0]
+  	end
+
 end
+
+
+
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,12 +1,27 @@
 require_relative '../lib/event'
+require_relative '../lib/ticket'
 
 describe Event do
-	let(:event) {described_class.new(1, [1,2], 10)}
+	let(:event) {described_class.new(1, [1,2], [1,2,3,4,5])}
+	let(:ticket){ double :ticket}
 
 	describe 'initializes an event' do
 		it 'exists' do
 			expect(event).to be_truthy
 		end
+
+		it 'initializes with an id' do
+			expect(event.id).to eq 1
+		end
+
+		it 'initializes with a location' do
+			expect(event.location).to eq [1,2]
+		end
+
+		it 'initializes with tickets' do
+			expect(event.tickets).to eq [1,2,3,4,5]
+		end
+
 		it 'calculates the Manhattan Distance between two points' do
 			expect(event.distance_to([0,0])).to eq 3
 		end
